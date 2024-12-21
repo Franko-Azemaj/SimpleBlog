bbcfcc7f3569c2fb725f627925ccf5fb1b23a257
Franko Azemaj <frankoazemaj@hotmail.com>
2024-12-21 15:46:38 +0100

    Add project files.

---
+++ SimpleBlog/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+
+        [HttpPost]
+        public IEnumerable<string> RegisterUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
